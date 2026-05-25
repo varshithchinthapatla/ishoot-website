a679719ccb70f45d0c13cf5cd00e141abed77e97
Varshith <varshithchinthapatla5@gmail.com>
2026-05-25 16:04:38 +0530

    abc

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -9,18 +9,41 @@ function Loader() {
 
       {/* Logo Animation */}
       <motion.h1
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{
-          opacity: 1,
-          scale: 1,
-        }}
-        transition={{
-          duration: 1.2,
-        }}
-        className="text-5xl md:text-7xl font-black tracking-[0.2em] text-white"
-      >
-        I_SH<span className="text-red-500">OO</span>TS
-      </motion.h1>
+  initial={{ opacity: 0, scale: 0.9 }}
+  animate={{ opacity: 1, scale: 1 }}
+  transition={{ duration: 1 }}
+  className="relative text-5xl md:text-7xl font-black tracking-[0.2em]"
+>
+
+  {/* Base Red Text */}
+  I_SH<span className="text-red-500">OO</span>TS
+
+  {/* Shine Layer */}
+  <span className="absolute inset-0 overflow-hidden">
+
+    <span
+      className="
+        absolute inset-0
+        text-transparent
+        bg-clip-text
+        bg-gradient-to-r
+        from-transparent
+        via-white
+        to-transparent
+        bg-[length:200%_100%]
+        animate-shine
+      "
+      style={{
+        WebkitBackgroundClip: "text",
+        WebkitTextFillColor: "transparent",
+      }}
+    >
+      I_SHOOTS
+    </span>
+
+  </span>
+
+</motion.h1>
 
     </div>
   );
